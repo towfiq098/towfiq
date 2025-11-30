3765b8b451a621767a43e47b343871430af517fc
towfiq098 <turakmunshi5@gmail.com>
2025-12-01 00:25:42 +0600

    Create 33) C Program to Check Whether a Number is Prime or Not.c

---
+++ 33) C Program to Check Whether a Number is Prime or Not.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n, flag = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if(n <= 1) flag = 0;
+    for(int i = 2; i <= n/2; i++) {
+        if(n % i == 0) { flag = 0; break; }
+    }
+    if(flag) printf("%d is Prime\n", n);
+    else printf("%d is Not Prime\n", n);
+    return 0;
+}
